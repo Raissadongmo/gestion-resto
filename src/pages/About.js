3093ac05410fe0348f9d118887ce02ce12159889
DONGMO Raissa <raissadongmo@gmail.com>
2022-09-02 10:16:00 +0100

    Appli De Presentation Des Pays Du Monde

---
+++ src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A Propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        aliquid sed fugiat voluptas? Odit animi beatae at fugiat, asperiores
+        voluptate exercitationem cum, minima deserunt minus quisquam perferendis
+        repellat dignissimos officia autem eum laudantium corrupti doloremque
+        alias quae ratione officiis iusto. Similique, nesciunt? Quis sit
+        voluptate dolor velit ratione deleniti libero expedita totam debitis
+        numquam, a provident repudiandae accusantium consequuntur saepe facere
+        dolore voluptatibus voluptatem, consequatur est architecto sequi!
+        Ratione cumque laboriosam suscipit, perspiciatis tempore, natus in
+        repudiandae impedit est consequatur doloribus nihil eligendi, laudantium
+        ullam. Ab suscipit exercitationem voluptate incidunt asperiores iste,
+        assumenda nobis aperiam, ducimus enim necessitatibus? Tempora, minus.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+        reprehenderit nisi nam porro in provident ut sed beatae architecto
+        consequatur aliquam, quibusdam at odio optio ad, modi consequuntur a
+        asperiores illo sequi ratione magni? Explicabo provident rem quam
+        voluptates enim nemo, praesentium vero repudiandae natus sunt asperiores
+        consequatur nisi voluptatem alias magnam ipsum odio unde, in quos
+        exercitationem aperiam totam! Accusantium omnis voluptates ex neque
+        accusamus ea dicta in! Laudantium consequuntur quas praesentium atque,
+        nihil quis, fugit quibusdam sunt voluptates cum provident porro eveniet
+        impedit deserunt nemo maiores incidunt mollitia beatae neque. Iure ipsam
+        accusantium labore voluptatum laudantium aliquid, minus quod iste
+        recusandae excepturi maiores, eaque incidunt itaque placeat aspernatur
+        velit inventore, earum at. Sequi quidem nihil itaque accusamus a sint
+        iure obcaecati nostrum inventore saepe, tempora labore voluptatem eos
+        dolore. Ab vitae similique magnam esse? Magni beatae quibusdam esse
+        aliquid illo praesentium laudantium quis maxime fuga mollitia nobis, aut
+        dicta sed. Quidem unde aperiam accusamus maiores obcaecati consectetur
+        perferendis ratione ipsum. Delectus quisquam optio veniam maxime
+        distinctio! Impedit harum, perspiciatis aspernatur illo consequuntur
+        exercitationem vero perferendis, blanditiis, doloribus amet voluptatum.
+        Ab odit consequuntur et. Amet sed aliquid, deserunt, beatae suscipit
+        facilis ut veniam modi dolorem nobis tenetur iste incidunt!
+      </p>
+    </div>
+  );
+};
+
+export default About;
